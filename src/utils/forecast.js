bad828fe2cd48c7ab893897242555c710ac410dc
Akshay Bandivadekar <c4.akshay.bandivadekar@gmail.com>
2021-12-12 17:18:58 +0530

    Added celcious in temprature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const forecast = (latitude, longitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=19f8224ad4e1d0f6b89873349c8ff69c&query=${latitude},${longitude}&units=f`;
+    const url = `http://api.weatherstack.com/current?access_key=19f8224ad4e1d0f6b89873349c8ff69c&query=${latitude},${longitude}&units=m`;
     request({url, json: true}, (error, { body }) => {
         if(error) {
             callback('Unable to connect to weather service!', undefined);
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined);
         } else {
             const {current} = body;
-            callback(undefined, `${current.weather_descriptions[0]}. It is currently ${current.temperature} degree out. It feels like ${current.feelslike} degree out. The humidity is ${current.humidity}%.`);
+            callback(undefined, `${current.weather_descriptions[0]}. It is currently ${current.temperature} degree celsius out. It feels like ${current.feelslike} degree celsius out. The humidity is ${current.humidity}%.`);
         }
     });
 };
